a3f42fb4b26a70427964d6b5280cfd40146b0463
Khushi Roy <khushinroy@gmail.com>
2025-07-28 10:57:33 +0530

    Add files via upload

---
+++ Loops/first.js
@@ -0,0 +1,112 @@
+//alert("Hello");one time message
+
+/*this is a comment*/
+//Arithmatic Operators
+//a=5
+//b=2
+/*console.log('a*b=',a*b);
+console.log('a+b=',a+b);
+console.log('a-b=',a-b);
+console.log('a%b=',a%b);
+console.log('a/b=',a/b);
+console.log('a**b=',a**b);
+console.log('a++=',a++);
+console.log('a=',a);
+console.log('++a=',++a); 
+console.log('a=',a);
+
+//Assignmanet Operators
+a += 4;//a=a+4
+console.log('a +=4 is a=',a);
+a /= 11//a=a/11
+console.log('a/11=',a);*/
+
+//comparion operators
+/*console.log("5==2" , a==b);
+console.log("a=!b" , a!=b);
+c="5"//string->number
+console.log("a===b" , a===b);
+console.log("a!==b" , a!==b); 
+
+//logical operators
+let cond1 = a<b;
+let cond2 = a===6;
+console.log("cond1 && cond2=", cond1 && cond2);
+console.log("6>5= && 4>3", 6>5 && 4>3);
+console.log("6>5= || 4<3", 6>5 || 4<3);
+console.log("!6>5= ", !(6>5));
+
+//comditional statements
+let mode= "light";
+let color;
+
+if (mode ==="dark"){
+    color="black";
+}
+else if(mode ==="light") {
+    color="White";
+}
+else{
+    color="red";
+}
+
+console.log(color);
+ 
+let num=10;
+if(num%2===0){
+    console.log("even");
+}
+else {
+    console.log("odd");
+} 
+//ternaray operator
+//  a?b:c 
+
+let Age=19;
+Age>18 ?"Adult": "Not Adult"
+
+// MDN Docs*/
+
+
+//practice question
+
+
+/*let a = prompt("enter the number");
+if (a%5===0){
+    console.log("divisible by 5");
+}
+else{
+    console.log("not divisible by 5");
+}*/
+
+
+let a= prompt("enter a number")
+let grade;
+if (a>=90 && a<=100){
+    grade="A";
+}
+else if(a>=70 && a<=89){
+     grade="B";
+}
+else if(a>=60 && a<=69){
+     grade="C";
+}
+else if(a>=50 && a<=59){
+     grade="D";
+}
+else {
+     grade="F"
+}
+
+console.log(" according to ur marks ur grade is:",grade)
+
+
+
+
+
+
+
+
+
+
+ 
