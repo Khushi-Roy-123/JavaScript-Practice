012a8fe78c2aa206baf0c95bb45d143a03a8f50c
Khushi Roy <khushinroy@gmail.com>
2025-08-01 01:43:13 +0530

    Add files via upload

---
+++ Object Litreals/script.js
@@ -0,0 +1,43 @@
+/*//Object Literals
+let student = {
+    name:"Khushi",
+    Class:12,
+    Marks:94.4,
+};
+
+//Object of Object
+const classInfo={
+    aman:{
+        grade:"A",
+        city:"Delhi",
+    },
+    Raman:{
+        grade:"B",
+        city:"Mumbai",
+    },
+    Chaman:{
+        grade:"C",
+        city:"Delhi",
+    }
+}
+
+const classData=[
+   {
+        grade:"A",
+        city:"Delhi",
+    },
+   {
+        grade:"B",
+        city:"Mumbai",
+    },
+    {
+        grade:"C",
+        city:"Delhi",
+    }
+]*/
+
+let num= Math.random();
+num = num*10;
+num=Math.floor(num);
+
+//Math.floor(Math.ranodom()*10)+1;
